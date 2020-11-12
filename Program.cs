4a60292f02232f64145f66eb22db5a9b477d63f7
Joyce <joycessilva17@hotmail.com>
2020-11-11 23:59:16 -0300

    Logica de Programação

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Logica_de_progamação
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //aqui vai o codigo
+
+            //Console.WriteLine("Digite seu nome");
+            //string nomeCompleto = Console.ReadLine();
+
+            //Console.WriteLine("Seja bem vindo "+nomeCompleto);
+            //fim
+
+            int valorUm;
+            int valorDois;
+
+            //entrada de dados
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("-------------------");
+            Console.Write("Digite o primeiro valor:");
+            valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Segundo valor:");
+            valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibir
+
+            Console.WriteLine("A soma de "+valorUm+" + "+valorDois+" = "+soma);
+        }
+
+            
+
+    }
+}
